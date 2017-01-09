7a2bc3cdaf0222fb90012f7b0ef3cc37c70b3060
yuryvn <yury.v.n@gmail.com>
2017-01-09 02:41:05 +0100

    added transistor and relay, note that relay switches on when In1=grd

--- TemperatureOverRadio/TemperatureOverRadio.ino
+++ TemperatureOverRadio/TemperatureOverRadio.ino
@@ -11,6 +11,13 @@ Now Radio module libraries
 #include <printf.h>
 
 
+//Transistor control
+
+int TransistorPin=4;
+
+
+
+
 //-----------------variables for temperature sensor---------------
 int OneWireBus=34;
 int numberOfDevices;
@@ -32,10 +39,9 @@ bool radioNumber = 0;
 RF24 radio(9,53); //9 is CE pin 53 is CSN
 
 
-byte addresses[][6] = {"1Node","2Node"};
+byte addresses[][6] = {"Toch1","Toch2"};
 
-// Used to control whether this node is sending or receiving
-bool role = 0;
+
 //-------------------------------------------------------------------------
 
 
@@ -49,7 +55,6 @@ float GetTemp(DallasTemperature &sens)
   int numberOfDevices=0;
   // call sensors.requestTemperatures() to issue a global temperature 
   // request to all devices on the bus
-  delay(3000);
   
   numberOfDevices = sens.getDeviceCount();
 
@@ -71,6 +76,11 @@ float GetTemp(DallasTemperature &sens)
 //SETUP
 void setup(void)
 {
+//------------------transistor setup------------------------
+  pinMode(TransistorPin, OUTPUT);
+  digitalWrite(TransistorPin,LOW);
+//----------------------------------------------------------
+  
   //--------------------------Temperature sensor initialization----------------------------
   // start serial port
   Serial.begin(115200);
@@ -88,8 +98,6 @@ void setup(void)
 
   //------------------------------------radio module initialization------------------------
   printf_begin();
-  Serial.println(F("RF24/examples/GettingStarted"));
-  Serial.println(F("*** PRESS 'T' to begin transmitting to the other node"));
   
   radio.begin();
 
@@ -101,117 +109,55 @@ void setup(void)
   radio.printDetails();
   
   // Open a writing and reading pipe on each radio, with opposite addresses
-  if(radioNumber){
-    radio.openWritingPipe(addresses[1]);
-    radio.openReadingPipe(1,addresses[0]);
-  }else{
-    radio.openWritingPipe(addresses[0]);
-    radio.openReadingPipe(1,addresses[1]);
-  }
+
+    radio.openWritingPipe(addresses[0]); //we will write to first adress, the first address will be RPIs
+    radio.openReadingPipe(1,addresses[1]);//this arduino address
+ 
   
   // Start the radio listening for data
   radio.startListening();
 
   //-------------------------------------------------------------------------------------
+Serial.println("Waiting for input");
 }//setup
 
 
 
 
 void loop() {
+
+   digitalWrite(TransistorPin,LOW);
+   delay(2000);
+   digitalWrite(TransistorPin,HIGH);
   
-  
-/****************** Ping Out Role ***************************/  
-if (role == 1)  {
+      unsigned long got_time;
+      float RequestedTemperature=0.0;
     
-    radio.stopListening();                                    // First, stop listening so we can talk.
-    
-    
-    Serial.println(F("Now sending"));
-
-    float Temperature = GetTemp(sensors);    // Take the time, and send it.  This will block until complete
-    unsigned long start_time=micros();
-    int transmitteddata=100;
-     if (!radio.write( &Temperature, sizeof(Temperature) )){
-       Serial.println(F("failed"));
-     }
-        
-    radio.startListening();                                    // Now, continue listening
-    
-    unsigned long started_waiting_at = micros();               // Set up a timeout period, get the current microseconds
-    boolean timeout = false;                                   // Set up a variable to indicate if a response was received or not
-    
-    while ( ! radio.available() ){                             // While nothing is received
-      if (micros() - started_waiting_at > 200000 ){            // If waited longer than 200ms, indicate timeout and exit while loop
-          timeout = true;
-          break;
-      }      
-    }
-        
-    if ( timeout ){                                             // Describe the results
-        Serial.println(F("Failed, response timed out."));
-    }else{
-        float RequestedTemperature;                                 // Grab the response, compare, and send to debugging spew
-        radio.read( &RequestedTemperature, sizeof(float) );
-        unsigned long end_time = micros();
-        
-        // Spew it
-        Serial.print(F("Sent measured Temperature="));
-        Serial.print(Temperature);
-        Serial.print(F(", Got response:RequestedTemperature="));
-        Serial.print(RequestedTemperature);
-        Serial.print(F(", Round-trip delay "));
-        Serial.print(end_time-start_time);
-        Serial.println(F(" microseconds"));
-    }
-
-    // Try again 5s later
-    delay(5000);
-  }
-
-
-
-/****************** Pong Back Role ***************************/
-
-  if ( role == 0 )
-  {
-    unsigned long got_time;
+  float Temperature = GetTemp(sensors);    // Take reading, and send it.  This will block until complete
     
     if( radio.available()){
-                                                                    // Variable for the received timestamp
-      while (radio.available()) {                                   // While there is data ready
-        radio.read( &got_time, sizeof(unsigned long) );             // Get the payload
+                                                                    // If receive radio signal
+      while (radio.available()) {                                   // 
+        radio.read( &RequestedTemperature, sizeof(float) );        //read requested temperature  
       }
+
      
-      radio.stopListening();                                        // First, stop listening so we can talk   
-      radio.write( &got_time, sizeof(unsigned long) );              // Send the final one back.      
-      radio.startListening();                                       // Now, resume listening so we catch the next packets.     
-      Serial.print(F("Sent response "));
-      Serial.println(got_time);  
-   }
- }
-
-
-
-
-/****************** Change Roles via Serial Commands ***************************/
-
-  if ( Serial.available() )
-  {
-    char c = toupper(Serial.read());
-    if ( c == 'T' && role == 0 ){      
-      Serial.println(F("*** CHANGING TO TRANSMIT ROLE -- PRESS 'R' TO SWITCH BACK"));
-      role = 1;                  // Become the primary transmitter (ping out)
+      radio.stopListening();                                        // First, stop listening so we can talk
+      //find out actual temperature at sensor and send it
+          Serial.println(F("---------------------------------------------"));
     
-   }else
-    if ( c == 'R' && role == 1 ){
-      Serial.println(F("*** CHANGING TO RECEIVE ROLE -- PRESS 'T' TO SWITCH BACK"));      
-       role = 0;                // Become the primary receiver (pong back)
-       radio.startListening();
-       
-    }
-  }
+    unsigned long start_time=micros();
+   // delay(1500);
+      if (!radio.write( &Temperature, sizeof(Temperature) )){  //send actual temperature over radio
+       Serial.println(F("failed"));
+     }
+    radio.startListening();
 
+    Serial.print(F("Requested Temperature="));Serial.println(RequestedTemperature);
+    Serial.print(F("Actual Temperature from Sensor="));Serial.println(Temperature);
+    Serial.print(F("Sent at time="));Serial.println(start_time); 
+    Serial.println(F("---------------------------------------------"));  
+   }
 
 } // Loop
 
